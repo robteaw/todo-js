834b91ccee8b6fef59ff1abbc69753174c50a827
robteaw <robteaw@gmail.com>
2023-07-13 18:39:30 -0500

    Todo Add Fix

--- main.js
+++ main.js
@@ -8,11 +8,6 @@ window.addEventListener("load", () => {
 
     const task = input.value;
 
-    if (!task) {
-      alert("Please fill out the task");
-      return;
-    }
-
     const task_el = document.createElement("div");
     task_el.classList.add("task");
 
@@ -29,24 +24,39 @@ window.addEventListener("load", () => {
 
     task_content_el.appendChild(task_input_el);
 
-    // add
     const task_actions_el = document.createElement("div");
     task_actions_el.classList.add("actions");
 
-    // edit
     const task_edit_el = document.createElement("button");
     task_edit_el.classList.add("edit");
-    task_edit_el.innerHTML = "Edit";
+    task_edit_el.innerText = "Edit";
 
-    // delete
     const task_delete_el = document.createElement("button");
     task_delete_el.classList.add("delete");
-    task_delete_el.innerHTML = "Delete";
+    task_delete_el.innerText = "Delete";
+
+    task_actions_el.appendChild(task_edit_el);
+    task_actions_el.appendChild(task_delete_el);
 
     task_el.appendChild(task_actions_el);
 
     list_el.appendChild(task_el);
 
     input.value = "";
+
+    task_edit_el.addEventListener("click", (e) => {
+      if (task_edit_el.innerText.toLowerCase() == "edit") {
+        task_edit_el.innerText = "Save";
+        task_input_el.removeAttribute("readonly");
+        task_input_el.focus();
+      } else {
+        task_edit_el.innerText = "Edit";
+        task_input_el.setAttribute("readonly", "readonly");
+      }
+    });
+
+    task_delete_el.addEventListener("click", (e) => {
+      list_el.removeChild(task_el);
+    });
   });
 });
